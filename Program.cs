364cf637dc4186e44977bbceff90edd60d5c9b98
regul951 <regul951@gmail.com>
2023-11-09 22:53:59 +0300

    Решение 2

--- Program.cs
+++ Program.cs
@@ -10,7 +10,19 @@ static int[] CreateRandomArray(int m, int minLimitRandom, int maxLimitRandom)
     return randomArray;
 }
 
-
+static int[,] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom)
+{
+    int[,] randomMatrix = new int[m, n];
+    Random rand = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            randomMatrix[i, j] = rand.Next(minLimitRandom, maxLimitRandom);
+        }
+    }
+    return randomMatrix;
+}
 
 static void PrintArray(int[] arr)
 {
@@ -22,6 +34,21 @@ static void PrintArray(int[] arr)
     Console.WriteLine();
 }
 
+static void PrintMatrix(int[,] matrix)
+{
+    int row = matrix.GetLength(0);
+    int column = matrix.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+
+
+}
 
 // 1. Реализовать сортировку выбором на одномерном массиве 
 // целых чисел. Сортировка выбором - найти максимум, 
@@ -30,11 +57,13 @@ static void PrintArray(int[] arr)
 // Пример - массив [6,2,0,9,7,1,3] должен выводится как [0,1,2,3,6,7,9]. 
 // Подумать, как реализовать сортировку по убыванию  [9,7,6,3,2,1,0]
 
-static void SortArrayUp(int[] arr)
+// int[] examArr = new int[] { 6, 2, 0, 9, 7, 1, 3 };
+// int[] rndArr = CreateRandomArray(7, -10, 10);
+
+static int[] SortArrayUp(int[] arr)
 {
     //Объявление переменных
     int arrLength = arr.Length;
-    int[] sortUp = arr;
     int indexMaxNumber = 0;
 
     //Сортировка
@@ -42,93 +71,131 @@ static void SortArrayUp(int[] arr)
     {
         for (int j = 1; j < arrLength + 1 - i; j++)
         {
-            if (sortUp[j] > sortUp[indexMaxNumber])
+            if (arr[j] > arr[indexMaxNumber])
             {
                 indexMaxNumber = j;
             }
         }
-        int tmpValue = sortUp[arrLength - i];
-        sortUp[arrLength - i] = sortUp[indexMaxNumber];
-        sortUp[indexMaxNumber] = tmpValue;
+        int tmpValue = arr[arrLength - i];
+        arr[arrLength - i] = arr[indexMaxNumber];
+        arr[indexMaxNumber] = tmpValue;
 
         indexMaxNumber = 0;
     }
     //Вывод в консоль отсортированного массива
-    Console.WriteLine("\nСортировка по возрастанию:");
-    PrintArray(sortUp);
+    // Console.WriteLine("\nСортировка по возрастанию:");
+    // PrintArray(arr);
 
-    // return sortUp;
+    return arr;
 }
 
+// static void SortArrayDown(int[] arr)
+// {
+//     //Объявление переменных
+//     int arrLength = arr.Length;
+//     int[] sortDown = arr;
+//     int indexMinNumber = 0;
+
+//     //Сортировка
+//     for (int i = 1; i < arrLength; i++)
+//     {
+//         for (int j = 1; j < arrLength + 1 - i; j++)
+//         {
+//             if (sortDown[j] < sortDown[indexMinNumber])
+//             {
+//                 indexMinNumber = j;
+//             }
+//         }
+//         int tmpValue = sortDown[arrLength - i];
+//         sortDown[arrLength - i] = sortDown[indexMinNumber];
+//         sortDown[indexMinNumber] = tmpValue;
+
+//         indexMinNumber = 0;
+//     }
+
+//     //Вывод в консоль отсортированного массива
+//     Console.WriteLine("\nСортировка по убыванию:");
+//     PrintArray(sortDown);
+
+//     // return sortDown;
+// }
 
 
-static void SortArrayDown(int[] arr)
-{
-    //Объявление переменных
-    int arrLength = arr.Length;
-    int[] sortDown = arr;
-    int indexMinNumber = 0;
 
-    //Сортировка
-    for (int i = 1; i < arrLength; i++)
-    {
-        for (int j = 1; j < arrLength + 1 - i; j++)
-        {
-            if (sortDown[j] < sortDown[indexMinNumber])
-            {
-                indexMinNumber = j;
-            }
-        }
-        int tmpValue = sortDown[arrLength - i];
-        sortDown[arrLength - i] = sortDown[indexMinNumber];
-        sortDown[indexMinNumber] = tmpValue;
-
-        indexMinNumber = 0;
-    }
-
-    //Вывод в консоль отсортированного массива
-    Console.WriteLine("\nСортировка по убыванию:");
-    PrintArray(sortDown);
-
-    // return sortDown;
-}
-
-int[] examArr = new int[] { 6, 2, 0, 9, 7, 1, 3 };
-int[] rndArr = CreateRandomArray(7, -10, 10);
-
-Console.WriteLine("Исходный массив:");
+// Console.WriteLine("Исходный массив:");
 // PrintArray(examArr);
 // SortArrayUp(examArr);
 // SortArrayDown(examArr);
 
-PrintArray(rndArr);
+// PrintArray(rndArr);
 // SortArrayUp(rndArr);
-SortArrayDown(rndArr);
+// SortArrayDown(rndArr);
 
 
-// 2. Реализовать сортировку выбором на двухмерном массиве построчно - отсортировать только строки
 
+// 2. Реализовать сортировку выбором на двухмерном массиве построчно - 
+// отсортировать только строки
 // Например:
-
 // 2 8 0 1
 // 3 8 2 5
 // 9 4 5 7
-
 // Станет:
-
 // 0 1 2 8
 // 2 3 5 8
 // 4 5 7 9
 
+static void SortMatrixRow(int[,] arr)
+{
+    int rows = arr.GetLength(0);
+    int columns = arr.GetLength(1);
 
-// 3. Реализовать сортировку выбором на двухмерном массиве - отсортировать все элементы
+    for (int i = 0; i < rows; i++)
+    {
+        //Пустой массив
+        int[] row = new int[columns];
 
+        //Извлечение строки 
+        for (int j = 0; j < columns; j++)
+        {
+            row[j] = arr[i, j];
+        }
+        //Сортировка строки
+        row = SortArrayUp(row);
+
+        //Замена отсортированным значением
+        for (int k = 0; k < columns; k++)
+        {
+            arr[i, k] = row[k];
+        }
+    }
+    //Вывод в консоль отсортированного массива
+    Console.WriteLine("\nСортировка строк матрицы по возрастанию:");
+    PrintMatrix(arr);
+}
+
+
+
+int[,] examMatrix = new int[,] { { 2, 8, 0, 1 }, { 3, 8, 2, 5 }, { 9, 4, 5, 7 } };
+int[,] rndMatrix = CreateRandomMatrix(3, 4, 0, 9);
+
+
+Console.WriteLine("Исходная матрица:");
+PrintMatrix(examMatrix);
+SortMatrixRow(examMatrix);
+
+// PrintMatrix(rndMatrix);
+// SortMatrixRow(rndMatrix);
+
+
+
+
+// 3. Реализовать сортировку выбором на двухмерном массиве - 
+// отсортировать все элементы
 // 2 8 0 1
 // 3 8 2 5
 // 9 4 5 7
-
 // Станет
-
 // 0 1 2 2
 // 3 4 5 5
 // 7 8 8 9
+
